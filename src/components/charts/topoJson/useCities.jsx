fa194ea1cf991b73ba3e8d54600b8234918f1c8f
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-01-05 18:05:16 +0300

    cities lat lng commit

--- src/components/charts/topoJson/useCities.jsx
+++ src/components/charts/topoJson/useCities.jsx
@@ -5,11 +5,18 @@ const csvUrl =
   "https://gist.githubusercontent.com/curran/13d30e855d48cdd6f22acdf0afe27286/raw/0635f14817ec634833bb904a47594cc2f5f9dbf8/worldcities_clean.csv";
 // "https://gist.githubusercontent.com/DERRICKSONNDAMBUKI/f71166cd23e0febe4e254dc15d9f8be5/raw/2539e6f40ce1ef50ca830f578a9e53032868a2eb/senseiworldcities.csv" // too large
 
+const row = (data) => {
+  data.lat = +data.lat;
+  data.lng = +data.lng;
+
+  return data;
+};
+
 export const useCities = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    csv(csvUrl).then((cities) => setData(cities));
+    csv(csvUrl, row).then(setData);
   }, []);
 
   return data;
